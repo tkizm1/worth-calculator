b593ee79dcc007857f2ec7765cba954de36a5436
Zylan <zihan.jian@outlook.com>
2025-03-14 10:23:44 +0800

    1

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,7 +42,7 @@ export default function RootLayout({
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
                   <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z" clipRule="evenodd" />
                 </svg>
-                在不同城市要过上同等生活水平的我到底需要多少钱？
+                另一个项目：在不同城市要过上同等生活水平需要多少钱？
               </a>
             </div>
           </div>
