f5748ece64d9a86f301d3d0051679c5d295eb170
Zylan <zihan.jian@outlook.com>
2025-03-19 16:05:00 +0800

    病假*0.6

--- components/calculator.tsx
+++ components/calculator.tsx
@@ -78,7 +78,7 @@ const SalaryCalculator = () => {
   const calculateWorkingDays = useCallback(() => {
     const weeksPerYear = 52;
     const totalWorkDays = weeksPerYear * Number(formData.workDaysPerWeek); // 确保转换为数字
-    const totalLeaves = Number(formData.annualLeave) + Number(formData.publicHolidays) + Number(formData.paidSickLeave);
+    const totalLeaves = Number(formData.annualLeave) + Number(formData.publicHolidays) + Number(formData.paidSickLeave) * 0.6; // 带薪病假按70%权重计算
     return Math.max(totalWorkDays - totalLeaves, 0);
   }, [formData.workDaysPerWeek, formData.annualLeave, formData.publicHolidays, formData.paidSickLeave]);
 
